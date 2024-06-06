8d689c7c48a2d476bf374626a2bd4bdd4207f2e9
Hosam-l <hosamhack00@gmail.com>
2024-06-06 12:36:13 +0300

    Create size_home.py

---
+++ size_home.py
@@ -0,0 +1,15 @@
+import os
+sys = os.system
+# Input 
+le = float(input ('please enter length: \n'))
+a3 = float(input ('please enter al3rd: \n'))
+m = float(input('how much meter: \n$'))
+#Names
+le = (le * a3)
+m = str(m * le)
+le = str(le)
+sys('clear')
+#Director
+print ('total meter : M' + le)
+print('total money : $' + m)
+#easy code python
